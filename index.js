25892af2974dda8edd056f2a34362d24ae008ff0
Makoto Henmi <mako.mail.g@gmail.com>
2020-02-26 16:21:08 +0900

    setting lint

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
 // 設定を.envからロード
-require('dotenv').config();
+require("dotenv").config();
 
 //ログイン処理
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const token = process.env.DISCORD_TOKEN;
 
-client.on('ready', () => {
-  console.log('ready...');
+client.on("ready", () => {
+  console.log("ready...");
 });
 
-client.on('message', message =>{
-  if(message.author.bot){
+client.on("message", message => {
+  if (message.author.bot) {
     return;
   }
   if (message.content.match(/ご安全に/)) {
-    let channel = message.channel;
-    let author = message.author.username;
-    let reply_text =`アクティブ体操やれ！ https://www.youtube.com/watch?v=KPxt7vyQ6Zo`;
-    message.reply(reply_text)
-        .then(message => console.log(`Sent message: ${reply_text}`))
-        .catch(console.error);
+    let reply_text = `アクティブ体操やれ！ https://www.youtube.com/watch?v=KPxt7vyQ6Zo`;
+    message
+      .reply(reply_text)
+      .then(() => console.log(`Sent message: ${reply_text}`))
+      .catch(console.error);
     return;
   }
 });
 
 client.login(token);
 
-const cron = require('node-cron');
+const cron = require("node-cron");
 
 // Create a new webhook
-const hock_id = process.env.DISCORD_HOCK_ID
-const hock_token = process.env.DISCORD_HOCK_TOKEN
+const hock_id = process.env.DISCORD_HOCK_ID;
+const hock_token = process.env.DISCORD_HOCK_TOKEN;
 const hook = new Discord.WebhookClient(hock_id, hock_token);
 
 // Send a message using the webhook
-cron.schedule('0 15 * * *', () => {
+cron.schedule("0 15 * * *", () => {
   var today = new Date();
   if (!isWeekend(today)) {
-    hook.send('みんなアクティブ体操やろう！ https://www.youtube.com/watch?v=KPxt7vyQ6Zo');
+    hook.send(
+      "みんなアクティブ体操やろう！ https://www.youtube.com/watch?v=KPxt7vyQ6Zo"
+    );
   }
 });
 
-function isWeekend(today){
-  var dayNum = today.getDay();//Date.getDay()は曜日番号として日曜始まりで0~6の値を返す
+function isWeekend(today) {
+  var dayNum = today.getDay(); //Date.getDay()は曜日番号として日曜始まりで0~6の値を返す
 
-  if(dayNum == 0 || dayNum == 6){
+  if (dayNum == 0 || dayNum == 6) {
     return true;
   }
 
