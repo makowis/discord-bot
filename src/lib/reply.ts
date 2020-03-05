93482dfedfc41d5ab8606a0e374fa30cecad3051
Makoto Henmi <mako.mail.g@gmail.com>
2020-03-05 20:58:26 +0900

    リファクタ

--- src/lib/reply.ts
+++ src/lib/reply.ts
@@ -1,6 +1,6 @@
-import * as Discord from 'discord.js'
+import { Message } from 'discord.js'
 
-const reply = (message: Discord.Message, reply_text: string) => {
+const reply = (message: Message, reply_text: string) => {
   message
   .reply(reply_text)
   .then(() => console.log(`Sent message: ${reply_text}`))
